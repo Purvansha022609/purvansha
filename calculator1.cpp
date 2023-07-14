39753c46fbc92644db6f8079a986fa345c29f8f8
Purvansha gehlod <139473344+Purvansha022609@users.noreply.github.com>
2023-07-14 09:40:19 +0530

    Add files via upload

---
+++ calculator1.cpp
@@ -0,0 +1,48 @@
+/* Name-Purvansha Gehlod
+PRN-22070123088
+*/
+#include<iostream>
+using namespace std; 
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int sum;
+int subtraction;
+int multiplication;
+int main()
+{
+    cout<<"enter first number";
+    cin>>a;
+    cout<<"enter second number";
+    cin>>b;
+    sum = a+b;
+    cout<<"the sum of two numbers is"<<sum;
+    cout<<"enter first number";
+    cin>>d;
+    cout<<"enter the second number";
+    cin>>e;
+    subtraction = d-e;
+    cout<<"the subtration of two number is"<<subtraction;
+    cout<<"enter the first number";
+    cin>>f;
+    cout<<"enter the second number";
+    cin>>g;
+    multiplication = f*g;
+    cout<<"the multiplication of two number is"<<multiplication<<endl;
+    
+}
+/*OUTPUT
+enter first number5
+enter second number5
+the sum of two numbers is 10
+enter first number5
+enter the second number5
+the subtration of two number is 0
+enter the first number2
+enter the second number2
+the multiplication of two number is 4
+*/
