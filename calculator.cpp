dca9811990087898db105fc787974b3f33d2b42a
Purvansha gehlod <139473344+Purvansha022609@users.noreply.github.com>
2023-07-14 09:07:03 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std; 
+int a;
+int b;
+int c;
+int sum;
+int main()
+{
+    cout<<"enter first number";
+    cin>>a;
+    cout<<"enter second number";
+    cin>>b;
+    sum = a+b;
+    cout<<"the sum of two numbers is"<<sum;
+}
---
+++ exp.cpp
@@ -0,0 +1,10 @@
+/*Name-Purvansha Gehlod
+PRN-22070123088*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"HELLO WORLD"<<endl;
+    cout<<"My first program";
+    return 0;
+} 
